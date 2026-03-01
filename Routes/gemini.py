7252bc8d002d5342982fc10c6986fa65f16a89c5
Smy250 <samyhopsu250@gmail.com>
2026-03-01 15:21:55 -0400

    Fix: Se ajustó que el método POST,reciba la información de la consulta.

--- Routes/gemini.py
+++ Routes/gemini.py
@@ -6,6 +6,6 @@ from Config.geminiConnection import chat as sesionGemini, consulta_agente_pro
 router = APIRouter()
 
 @router.post("/respuesta_ia/")
-def obtener_metodo_pago(respuesta: str, db: Session = Depends(get_db)):
-  res = consulta_agente_pro("", respuesta, sesionGemini)
+def obtener_metodo_pago(info: dict, db: Session = Depends(get_db)):
+  res = consulta_agente_pro("", info['respuesta'], sesionGemini)
   return res
