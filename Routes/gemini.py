20efacbba6f521f913270165c1f95c5bb642734a
Smy250 <samyhopsu250@gmail.com>
2026-03-05 21:51:39 -0400

    Fix: Se han eliminado rutas sin uso.

--- Routes/gemini.py
+++ Routes/gemini.py
@@ -16,35 +16,6 @@ from tensoflow.recomendations import entrenar_y_predecir
 
 router = APIRouter()
 
-#Gemini obtiene las ventas totales y responde con un analisis.
-@router.post("/respuesta_ia/ventas_totales/")
-def obtener_ventas_totales_ia(info: dict, db: Session = Depends(get_db), skip: int = 0, 
-  limit: int = 100,):
-  resultado = (
-    db.query(
-      Platillo.nombre.label("nombre_platillo"),
-      DetallePago.cantidad.label("cantidad_vendida"),
-      MetodoPago.nombre.label("metodo_pago"),
-      (Platillo.precio * DetallePago.cantidad).label("precio_total")
-    )
-    .join(DetallePago, DetallePago.id_platillo == Platillo.id)
-    .join(RegistroDePagos, RegistroDePagos.id == DetallePago.id_pago)
-    .join(MetodoPago, MetodoPago.id == RegistroDePagos.id_metodo_pago)
-    .offset(skip)
-    .limit(limit)
-    .all()
-  )
-  ventas_dict = [row._asdict() for row in resultado]
-  
-  lineas = []
-  for venta in ventas_dict:
-    linea = f"Platillo: {venta['nombre_platillo']}, Cantidad: {venta['cantidad_vendida']}, Método: {venta['metodo_pago']}, Total: ${venta['precio_total']:.2f}"
-    lineas.append(linea)
-  clave_para_analisis = "\n".join(lineas)
-  
-  consulta_gemini = consulta_agente_pro(clave_para_analisis, info['respuesta'], sesionGemini)
-  return consulta_gemini
-
 #Gemini obtiene todas las estadisticas para asesorar al usuario.
 @router.post("/respuesta_ia/")
 def obtener_ventas_totales_ia(info: dict):
@@ -77,11 +48,9 @@ def obtener_ventas_totales(db: Session = Depends(get_db), skip: int = 0,
       Platillo.nombre.label("nombre_platillo"),
       DetallePago.cantidad.label("cantidad_vendida"),
       MetodoPago.nombre.label("metodo_pago"),
-      (Platillo.precio * DetallePago.cantidad).label("precio_total")
     )
     .join(DetallePago, DetallePago.id_platillo == Platillo.id)
     .join(RegistroDePagos, RegistroDePagos.id == DetallePago.id_pago)
-    .join(MetodoPago, MetodoPago.id == RegistroDePagos.id_metodo_pago)
     .offset(skip)
     .limit(limit)
     .all()
@@ -105,41 +74,6 @@ def obtener_ventas_totales(db: Session = Depends(get_db),):
   
   return {"ventas_totales":suma_total} 
 
-"""
-Retorna los productos más vendidos (top N) con:
-- Nombre del platillo
-- Cantidad total vendida (sumada de todas las ventas)
-"""
-@router.post("/respuesta_ia/productos-top3")
-def obtener_top3_productos_mas_vendidos_ia(
-  info: dict,
-  limite: int = 3,
-  db: Session = Depends(get_db)
-):
-  resultado = (
-    db.query(
-      Platillo.nombre.label("nombre_platillo"),
-      func.sum(DetallePago.cantidad).label("cantidad_total_vendida"),
-      func.sum(Platillo.precio * DetallePago.cantidad).label("ventas_acumuladas")
-    )
-    .join(DetallePago, DetallePago.id_platillo == Platillo.id)
-    .group_by(Platillo.id, Platillo.nombre)
-    .order_by(desc("cantidad_total_vendida"))
-    .limit(limite)
-    .all()
-  )
-  
-  ventas_dict = [row._asdict() for row in resultado]
-  
-  lineas = []
-  for venta in ventas_dict:
-    linea = f"Nombre del Platillo: {venta['nombre_platillo']}, Cantidad Vendidas Totales: {venta['cantidad_total_vendida']}"
-    lineas.append(linea)
-  
-  clave_para_analisis = "\n".join(lineas)
-  
-  consulta_gemini = consulta_agente_pro(clave_para_analisis, info['respuesta'], sesionGemini)
-  return consulta_gemini
 
 """
 Retorna los productos más vendidos (top 3) con los parametros:
@@ -151,7 +85,6 @@ def obtener_top3_productos_mas_vendidos(
   limite: int = 3,
   db: Session = Depends(get_db)
 ):
-  
   
   resultado = (
     db.query(
